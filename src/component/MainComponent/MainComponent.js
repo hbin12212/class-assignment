6870d6b9b4f0b3035353479d69920a991f6932e0
김효빈 <52522662+sg05138@users.noreply.github.com>
2021-01-20 16:16:46 +0900

    [STYLE] : 뷰 스타일링

--- src/component/MainComponent/MainComponent.js
+++ src/component/MainComponent/MainComponent.js
@@ -14,7 +14,7 @@ const MainComponent = () => {
                     <div className="school-name">통진중학교</div>
                     <div className="information">
                         <div id="info-header">정보입력</div>
-                        <div id="info-content">본인의 생년월일과 이름을 입력해주세요.</div>
+                        {/* <div id="info-content">본인의 생년월일과 이름을 입력해주세요.</div> */}
 
                         <div className="input-box">
                             <InputBox />
--- src/molecule/InputBox/InputBox.js
+++ src/molecule/InputBox/InputBox.js
@@ -1,27 +1,54 @@
 import React, { useState, useEffect } from "react";
 //reactstrap
-import { Form, FormGroup, Label, Input, FormFeedback, FormText } from "reactstrap";
-//icon
-import CakeRoundedIcon from "@material-ui/icons/CakeRounded";
+import { Input, FormFeedback, Container, FormGroup } from "reactstrap";
+
+//scss
+import "./InputBox.scss";
 
 const InputBox = () => {
-    const [birth, setBirth] = useState("940107");
-    const [name, setName] = useState("이정환");
+    const [birth, setBirth] = useState("");
+    const [name, setName] = useState("");
 
     return (
-        <div className="InputBox">
-            <div className="birth-wrapper">
-                <CakeRoundedIcon />
-                <Input valid /> <FormFeedback tooltip>6자리의 숫자만 입력 가능합니다.</FormFeedback>
+        <Container>
+            <div className="InputBox">
+                <div className="birth-wrapper">
+                    <div className="label">
+                        <div id="birth-icon">🎂</div>
+                        <span> 생년월일 6자리를 입력해주세요</span>
+                    </div>
+                    <FormGroup className="position-relative">
+                        <Input
+                            valid
+                            value={birth}
+                            className="input-box"
+                            placeholder="ex) 981231"
+                            onChange={({ target: { value } }) => setBirth(value)}
+                        />
+                        <FormFeedback valid tooltip>
+                            6자리의 숫자만 입력 가능합니다.
+                        </FormFeedback>
+                    </FormGroup>
+                </div>
+                <br />
+                <div className="name-wrapper">
+                    <div className="label">
+                        <div id="name-icon">📚</div>
+                        <span> 이름을 입력해주세요</span>
+                    </div>
+                    <FormGroup className="position-relative">
+                        <Input
+                            invalid
+                            value={name}
+                            className="input-box"
+                            placeholder="ex) 김희수"
+                            onChange={({ target: { value } }) => setName(value)}
+                        />
+                        <FormFeedback tooltip>Sweet! that name is available</FormFeedback>
+                    </FormGroup>
+                </div>
             </div>
-            <br />
-            <div className="name-wrapper">
-                <Input invalid />
-                <FormFeedback valid tooltip>
-                    Sweet! that name is available
-                </FormFeedback>
-            </div>
-        </div>
+        </Container>
     );
 };
 
